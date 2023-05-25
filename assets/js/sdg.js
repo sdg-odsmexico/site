d7892fe94ab5e2c884f6cc0fae6ff072dbdbde2c
SandraMarq <118024145+SandraMarq@users.noreply.github.com>
2023-05-25 20:54:19 +0000

    Deploying to gh-pages from  @ 9b5f7cea8795d4f1077610aab55e9e512cc2ea57 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1175,7 +1175,7 @@ $(document).ready(function() {
     });
 });
 opensdg.chartColors = function(indicatorId) {
-  var colorSet = "classic";
+  var colorSet = "accesible";
   var numberOfColors = 0;
   var customColorList = [];
 
