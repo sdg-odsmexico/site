346e6c39b4ed366e3c87394812bca961bc080423
SandraMarq <118024145+SandraMarq@users.noreply.github.com>
2023-06-05 21:35:33 +0000

    Deploying to gh-pages from  @ cfaaab38249be2a77b39c526a8bf584c34f812e9 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3147,7 +3147,7 @@ var mapView = function () {
     $('.map').show();
     $('#map').sdgMap({
       indicatorId: indicatorId,
-      mapOptions: {"disaggregation_controls":true,"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"mapColors.default","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
+      mapOptions: {"disaggregation_controls":true,"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"opensdg.mapColors.default","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
       mapLayers: [{"subfolder":"map/entidades-federativas","label":"indicator.map","min_zoom":0,"max_zoom":6,"staticBorders":true},{"subfolder":"map/municipios","label":"indicator.map","min_zoom":7,"max_zoom":20,"staticBorders":false}],
       precision: precision,
       precisionItems: precisionItems,
