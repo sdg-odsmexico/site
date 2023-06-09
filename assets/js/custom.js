c409fe5545433e726a043fcf5f6acc94a3b826e4
SandraMarq <118024145+SandraMarq@users.noreply.github.com>
2023-06-09 09:45:26 -0600

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,9 @@
-// Add any custom javascript here.
+// Add any custom javafuscript here.
+function myMapColorFunction(indicatorId, goalId) {
+  if (goalId == 3) {
+    return chroma.brewer.OrRd;
+  }
+  else {
+    return chroma.brewer.BuGn;
+  }
+}
